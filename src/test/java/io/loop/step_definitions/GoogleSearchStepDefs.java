8e07eaa55cb7ca2c2932bd0b48af93d44f4bce29
Ketevan Karchava <ketevan0915@gmail.com>
2025-02-18 19:44:37 -0700

    second comit

---
+++ src/test/java/io/loop/step_definitions/GoogleSearchStepDefs.java
@@ -0,0 +1,48 @@
+package io.loop.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.loop.pages.GoogleSearchPage;
+import io.loop.utils.BrowserUtils;
+import io.loop.utils.ConfigurationReader;
+import io.loop.utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.Assert.assertEquals;
+
+public class GoogleSearchStepDefs {
+
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
+    @Given("user is on Google search page")
+    public void user_is_on_google_search_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperties("google.url"));
+        BrowserUtils.takeScreenshot();
+    }
+
+
+    @When("user types Loop Academy in the google search box and clicks enter")
+    public void user_types_loop_academy_in_the_google_search_box_and_clicks_enter() throws InterruptedException {
+        googleSearchPage.searchBox.sendKeys("Loop Academy" + Keys.ENTER);
+
+        WebElement element = Driver.getDriver().findElement(By.xpath("//iframe[@title='reCAPTCHA']"));
+        Driver.getDriver().switchTo().frame(element);
+        if(googleSearchPage.captcha.isDisplayed()){
+            googleSearchPage.captcha.click();
+        }
+        Driver.getDriver().switchTo().defaultContent();
+
+        Thread.sleep(3000);
+    }
+
+
+    @Then("user should see Loop Academy - Google search in the google title")
+    public void user_should_see_loop_academy_google_search_in_the_google_title() {
+        String actualTitle = Driver.getDriver().getTitle();
+        assertEquals("Expected result doesn't match actual result", "Loop Academy - Google Search", actualTitle);
+    }
+
+}
--- src/test/java/io/loop/step_definitions/LoginStepDefs.java
+++ src/test/java/io/loop/step_definitions/LoginStepDefs.java
@@ -3,16 +3,108 @@ package io.loop.step_definitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.loop.pages.LoginPage;
+import io.loop.utils.BrowserUtils;
+import io.loop.utils.ConfigurationReader;
+import io.loop.utils.DocuportConstants;
+import io.loop.utils.Driver;
+
+import static org.junit.Assert.assertTrue;
 
 public class LoginStepDefs {
+
+    LoginPage loginPage = new LoginPage();
+
     @Given("user is on Docuport login page")
     public void user_is_on_docuport_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Driver.getDriver().get(ConfigurationReader.getProperties("docuportBETA"));
     }
     @When("user enters username for client")
     public void user_enters_username_for_client() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        BrowserUtils.waitForClickable(LoginPage.loginButton, DocuportConstants.LARGE);
+        assertTrue("Login button is not displayed", loginPage.loginButton.isDisplayed());
+        loginPage.usernameInput.sendKeys(DocuportConstants.USERNAME_CLIENT);
     }
+
+    @When("user enters password for client")
+    public void user_enters_password_for_client() {
+        loginPage.passwordInput.sendKeys(DocuportConstants.PASSWORD);
+    }
+
+    @When("user clicks login button")
+    public void user_clicks_login_button() {
+        loginPage.loginButton.click();
+    }
+
+    @Then("user should be able to see the home for client")
+    public void user_should_be_able_to_see_the_home_for_client() {
+        assertTrue("Home button is not displayed", loginPage.homeButton.isDisplayed());
+    }
+
+
+
+
+
+
+    @When("user enters username for employee")
+    public void user_enters_username_for_employee() {
+        BrowserUtils.waitForClickable(LoginPage.loginButton, DocuportConstants.LARGE);
+        assertTrue("Login button is not displayed", loginPage.loginButton.isDisplayed());
+        loginPage.usernameInput.sendKeys(DocuportConstants.USERNAME_EMPLOYEE);
+    }
+
+    @When("user enters password for employee")
+    public void user_enters_password_for_employee() {
+        loginPage.passwordInput.sendKeys(DocuportConstants.PASSWORD);
+    }
+
+    @Then("user should be able to see the home for employee")
+    public void user_should_be_able_to_see_the_home_for_employee() {
+        assertTrue("Home button is not displayed", loginPage.homeButton.isDisplayed());
+    }
+
+
+
+
+
+    @When("user enters username for advisor")
+    public void user_enters_username_for_advisor() {
+        BrowserUtils.waitForClickable(LoginPage.loginButton, DocuportConstants.LARGE);
+        assertTrue("Login button is not displayed", loginPage.loginButton.isDisplayed());
+        loginPage.usernameInput.sendKeys(DocuportConstants.USERNAME_ADVISOR);
+    }
+
+    @When("user enters password for advisor")
+    public void user_enters_password_for_advisor() {
+        loginPage.passwordInput.sendKeys(DocuportConstants.PASSWORD);
+    }
+
+    @Then("user should be able to see the home for advisor")
+    public void user_should_be_able_to_see_the_home_for_advisor() {
+        assertTrue("Home button is not displayed", loginPage.homeButton.isDisplayed());
+    }
+
+
+
+
+
+    @When("user enters username for supervisor")
+    public void user_enters_username_for_supervisor() {
+        BrowserUtils.waitForClickable(LoginPage.loginButton, DocuportConstants.LARGE);
+        assertTrue("Login button is not displayed", loginPage.loginButton.isDisplayed());
+        loginPage.usernameInput.sendKeys(DocuportConstants.USERNAME_SUPERVISOR);
+    }
+
+    @When("user enters password for supervisor")
+    public void user_enters_password_for_supervisor() {
+        loginPage.passwordInput.sendKeys(DocuportConstants.PASSWORD);
+    }
+
+    @Then("user should be able to see the home for supervisor")
+    public void user_should_be_able_to_see_the_home_for_supervisor() {
+        assertTrue("Home button is not displayed", loginPage.homeButton.isDisplayed());
+    }
+
+
+
 }
